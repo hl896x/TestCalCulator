e01e83627cb711b4470de3d04b4c1edd577238e8
hl896 <hl896@uowmail.edu.au>
2022-03-16 14:29:53 +1100

    deploy code

---
+++ build/precache-manifest.43dc4ad8e4c83c8e24dc5232088527f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "06bc41f85fdc74f93412",
+    "url": "/TestCalCulator/static/css/main.917610ac.chunk.css"
+  },
+  {
+    "revision": "06bc41f85fdc74f93412",
+    "url": "/TestCalCulator/static/js/main.06bc41f8.chunk.js"
+  },
+  {
+    "revision": "5802f611e5186a579bdf",
+    "url": "/TestCalCulator/static/js/runtime~main.5802f611.js"
+  },
+  {
+    "revision": "c176bbacec31bb69a946",
+    "url": "/TestCalCulator/static/js/2.c176bbac.chunk.js"
+  },
+  {
+    "revision": "303f9c7c88d1f091ca64fc1aaeaf62cc",
+    "url": "/TestCalCulator/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c33bf28dcbd297c174e0aa44300f45fa.js"
+  "/TestCalCulator/precache-manifest.43dc4ad8e4c83c8e24dc5232088527f5.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/TestCalCulator/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
